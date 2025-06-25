66ed191e7c50b101a9212cc545005dd0e03bdd1c
DucksAreVeryCool <160318758+DucksAreVeryCoolDev@users.noreply.github.com>
2025-06-25 17:47:11 +0100

    Add Mod Template
    Cool

---
+++ src/client/java/duckzone/morestructures/MoreStructuresClient.java
@@ -0,0 +1,10 @@
+package duckzone.morestructures;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoreStructuresClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
